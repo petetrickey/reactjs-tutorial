c39a82d5e5fd58581f6314ec98260ee51359b843
Pete Trickey <7979745+petetrickey@users.noreply.github.com>
2020-02-01 23:13:31 +0000

    ES6 function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square" onClick={function() { alert('clicked')}}>
+            <button className="square" onClick={() => { alert('clicked')}}>
                 {this.props.value}
             </button>
         );
