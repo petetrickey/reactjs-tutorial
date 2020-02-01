c8240409e1523de57c77e996608932b86cc7e3cd
Pete Trickey <7979745+petetrickey@users.noreply.github.com>
2020-02-01 23:31:06 +0000

    Add state

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: props.value,
+        }
+    }
+
     render() {
         return (
-            <button className="square" onClick={() => { alert('clicked')}}>
-                {this.props.value}
+            <button
+                className="square"
+                onClick={() => { this.setState({value: 'X'})}}>
+                {this.state.value}
             </button>
         );
     }
