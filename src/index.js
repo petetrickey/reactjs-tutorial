b12d878f685168fac9bd00d41c244184dc51ab6f
Pete Trickey <7979745+petetrickey@users.noreply.github.com>
2020-02-01 23:24:00 +0000

    Functional component rather than class component. Why is react tutorial still class component?

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square" onClick={() => { alert('clicked')}}>
-                {this.props.value}
-            </button>
-        );
-    }
+const Square = ({value}) => {
+    return (
+        <button className="square" onClick={() => { alert('clicked')}}>
+            {value}
+        </button>
+    )
 }
 
 class Board extends React.Component {
